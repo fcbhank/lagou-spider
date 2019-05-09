20db39a70629bd0057ace6cd2e15bc9ff73ca9dd
hank <hankhu163@foxmail.com>
2019-05-09 16:20:23 +0800

    finished it

--- src/main/java/LagouSpider.java
+++ src/main/java/LagouSpider.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+
 /**
  * Create by root on 2019/5/8
  */
@@ -18,11 +20,41 @@ public class LagouSpider {
         // 设置爬取的页面
         webDriver.get("https://www.lagou.com/zhaopin/Java/?labelWords=label");
 
+        // 设置筛选条件
         clickOption(webDriver, "工作经验", "应届毕业生");
         clickOption(webDriver, "学历要求", "本科");
         clickOption(webDriver, "融资阶段", "不限");
-        clickOption(webDriver, "公司规模", "150-500人");
-        clickOption(webDriver, "行业领域", "不限");
+        clickOption(webDriver, "公司规模", "不限");
+        clickOption(webDriver, "行业领域", "移动互联网");
+
+        // 解析页面元素
+        analysisByPage(webDriver);
+
+    }
+
+    private static void analysisByPage(WebDriver webDriver) {
+        List<WebElement> jobElements = webDriver.findElements(By.className("con_list_item "));
+        for (WebElement jobElement : jobElements) {
+            String company = jobElement.findElement(By.className("company")).findElement(By.className("company_name")).
+                    findElement(By.xpath("a")).getText();
+            String money = jobElement.findElement(By.className("position")).findElement(By.className("money")).getText();
+            System.out.println(company + " : " + money);
+        }
+
+        // 获取页面顶部下一页按钮
+        WebElement nextPageBtn = webDriver.findElement(By.className("pager_next "));
+        // 不是最后一页
+        if (!nextPageBtn.getAttribute("class").contains("pager_next_disabled")) {
+            nextPageBtn.click();
+            System.out.println("\n----解析下一页----");
+            // 等待网页更新加载出来，否则找不到对应的element
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            analysisByPage(webDriver);
+        }
     }
 
     private static void clickOption(WebDriver webDriver, String chosenTitle, String optionTitle) {
@@ -32,5 +64,6 @@ public class LagouSpider {
         WebElement optionElement = chosenElement.findElement(By.xpath("../a[contains(text(), '" + optionTitle + "')]"));
         // 点击选中元素
         optionElement.click();
+        // 关闭driver
     }
 }
